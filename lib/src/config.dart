6d7c92e9f8159f355d88448907b9fc62eb17ea68
Phạm Ngọc Thắng <thangpn@tastech.asi>
2023-11-22 16:10:04 +0700

    add media kit

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -1,8 +1,10 @@
+import 'package:video_player_media_kit/video_player_media_kit.dart';
+
 class MediaEditorConfig {
-  // static Future<void> configMediaKit() async {
-  //   VideoPlayerMediaKit.ensureInitialized(
-  //     android: true,
-  //     iOS: true,
-  //   );
-  // }
+  static Future<void> configMediaKit() async {
+    VideoPlayerMediaKit.ensureInitialized(
+      android: true,
+      iOS: true,
+    );
+  }
 }
