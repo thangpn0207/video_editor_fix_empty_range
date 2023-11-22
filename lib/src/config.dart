c185425f3910f9b306cb7e381afc011c46a2f4d5
Phạm Ngọc Thắng <thangpn@tastech.asi>
2023-11-22 16:26:56 +0700

    update

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -1,10 +1,10 @@
-import 'package:video_player_media_kit/video_player_media_kit.dart';
-
-class MediaEditorConfig {
-  static Future<void> configMediaKit() async {
-    VideoPlayerMediaKit.ensureInitialized(
-      android: true,
-      iOS: true,
-    );
-  }
-}
+// import 'package:video_player_media_kit/video_player_media_kit.dart';
+//
+// class MediaEditorConfig {
+//   static Future<void> configMediaKit() async {
+//     VideoPlayerMediaKit.ensureInitialized(
+//       android: true,
+//       iOS: true,
+//     );
+//   }
+// }
